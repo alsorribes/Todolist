1d01178744bb39f5cb432c73558b268e8dac7303
Albert <alsorribes@gmail.com>
2025-07-02 13:01:31 +0200

    Implementació de component app sencer

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,48 +1,15 @@
-//Classe app per importar i utilitzar el servei
-
-import { Component, inject } from '@angular/core';
+// Componente principal de la aplicación
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { TaskService } from './services/task.service';
+import { TaskForm } from './components/task-form/task-form';
+import { TaskList } from './components/task-list/task-list';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TaskForm, TaskList],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-
 export class App {
-  protected title = 'Todolist';
-
-  //Injectem el servei
-  private taskService = inject(TaskService);
-
-  //Propietats pel formulari
-  newTaskTitle = '';
-  newTaskDescription = '';
-
-  //Exposem les tasques del servei
-  get tasks() {
-    return this.taskService.tasks();
-  }
-
-  //Mètode per afegir una nova tasca
-  addTask() {
-    if (this.newTaskTitle.trim()) {
-      this.taskService.addTask(this.newTaskTitle, this.newTaskDescription);
-      this.newTaskTitle = '';
-      this.newTaskDescription = '';
-    }
-  }
-
-  //Mètode per marcar/desmarcar com a completada
-  compTask(taskId: number) {
-    this.taskService.completedTask(taskId);
-  }
-
-  //Mètode per eliminar una tasca
-  deleteTask(taskId: number) {
-    this.taskService.deleteTask(taskId);
-  }
-
+  protected title = 'TodoList';
 }

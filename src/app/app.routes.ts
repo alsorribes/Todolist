10abe90dca033b2c8149e422f67599d9a5b2113d
Albert <alsorribes@gmail.com>
2025-07-04 10:32:31 +0200

    SetUp de vista /edit (conté errors encara)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,9 +4,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { About } from './components/about/about';
 import { Home } from './components/home/home';
 import { Completed } from './components/completed/completed';
+import { Edit } from './components/edit/edit';
 
 export const routes: Routes = [
-    { path: '', component: Home}, //Només podem tenir un path amb '' que serà el que gestionarà tot (arrel)
+    { path: '', component: Home }, //Només podem tenir un path amb '' que serà el que gestionarà tot (arrel)
     { path: 'about', component: About }, //Ruta per la vista del component /about
-    { path: 'completed', component: Completed} //Ruta per la vista del component /completed
+    { path: 'completed', component: Completed }, //Ruta per la vista del component /completed
+    { path: 'edit/:id', component: Edit} //Ruta per la vista del component /edit (:id -> indica que es un paràmetre dinamic)
 ];
--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { TaskList } from '../task-list/task-list';
 import { TaskForm } from '../task-form/task-form';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [TaskForm, TaskList],
+  imports: [TaskForm, TaskList, CommonModule],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })

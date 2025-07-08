3f39f15532c310572f5c0dc6ee6ec086de00c619
Albert <alsorribes@gmail.com>
2025-07-08 12:36:38 +0200

    Millores visuals per la selecció d'usuaris

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -6,19 +6,47 @@ import { TaskService } from '../../services/task.service';
 import { Task } from '../../models/task.model';
 import { RouterModule } from '@angular/router';
 import { User } from "../user/user";
+import { Observable } from 'rxjs';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, RouterModule, TaskForm, TaskList, User],
+  imports: [CommonModule, RouterModule, TaskForm, TaskList],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
 export class Home {
   protected title = 'ToDoList';
   tasks: Task[] = [];
+  name$!: Observable<string>;
+  userMenuOpen = false;
 
-  constructor(private taskService: TaskService) {
+  constructor(private taskService: TaskService, private userService: UserService) {
     this.tasks = this.taskService.getPendingTasks();
+  }
+
+  ngOnInit(): void {
+    // Ens subscrivim al nom actual de l'usuari
+    this.name$ = this.userService.name$;
+  }
+
+  toggleUserMenu(): void {
+    this.userMenuOpen = !this.userMenuOpen;
+  }
+
+  setToJoan(): void {
+    this.userService.setNameToJoan();
+    this.userMenuOpen = false;
+  }
+
+  setToJordi(): void {
+    this.userService.setNameToJordi();
+    this.userMenuOpen = false;
+  }
+
+  setToAlba(): void {
+    this.userService.setNameToAlba();
+    this.userMenuOpen = false;
   }
 }

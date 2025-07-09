821f84b8a194f696be37674fcb8a87f318999092
Albert <alsorribes@gmail.com>
2025-07-09 13:23:51 +0200

    Creació de finestra 'colors' per practicar el css en profunditat

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,10 +5,12 @@ import { About } from './components/about/about';
 import { Home } from './components/home/home';
 import { Completed } from './components/completed/completed';
 import { Edit } from './components/edit/edit';
+import { Colors } from './components/colors/colors';
 
 export const routes: Routes = [
     { path: '', component: Home }, //Només podem tenir un path amb '' que serà el que gestionarà tot (arrel)
     { path: 'about', component: About }, //Ruta per la vista del component /about
     { path: 'completed', component: Completed }, //Ruta per la vista del component /completed
-    { path: 'edit/:id', component: Edit} //Ruta per la vista del component /edit (:id -> indica que es un paràmetre dinamic)
+    { path: 'edit/:id', component: Edit}, //Ruta per la vista del component /edit (:id -> indica que es un paràmetre dinamic)
+    { path: 'colors', component: Colors } //Ruta per la vista del component /colors per practicar css
 ];
---
+++ src/app/components/colors/colors.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-colors',
+  imports: [],
+  templateUrl: './colors.html',
+  styleUrl: './colors.css'
+})
+export class Colors {
+
+}

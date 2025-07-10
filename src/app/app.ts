fb1178eaa4ac5925f29f7a80c71b7b27aeb5016a
Albert <alsorribes@gmail.com>
2025-07-10 12:20:46 +0200

    Configuració de ionic framework

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,11 +3,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormsModule, RouterModule], //Feia falta importar RouterModule perque el botó de Sobre fos clicable, perquè Angular sino no reconeix routerLink (a app.html) i ho renderitza com a text.
+  imports: [RouterOutlet, FormsModule, RouterModule, IonicModule], //Feia falta importar RouterModule perque el botó de Sobre fos clicable, perquè Angular sino no reconeix routerLink (a app.html) i ho renderitza com a text.
   templateUrl: './app.html',
   styleUrls: ['./app.css']
 })
--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,8 @@ import { appConfig } from './app/app.config';
 import { App } from './app/app';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
+import { provideIonicAngular } from '@ionic/angular/standalone';
 
 bootstrapApplication(App, {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), provideIonicAngular()]
 });
